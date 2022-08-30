94d7445ca2eb41d123079a2b8aad42871701912c
juliotorres10 <111753831+juliotorres10@users.noreply.github.com>
2022-08-30 11:55:59 -0300

    jscript abrir e fechar menu

--- js/code.js
+++ js/code.js
@@ -1,2 +1,38 @@
 //Códigos para o Hot site Rubem Braga
 
+//Cria um novo objeto da classe date 
+const tempo = new Date()
+const txtData = tempo.getFullYear() + ' - hora: '+ tempo.getHours() + ' minuto: ' + tempo.getMinutes()
+
+//ELEMENTOS DOM
+const spanData = document.getElementById('data').innerText = txtData
+const btFechar = document.getElementById('btFechar')
+const divModal = document.getElementById('divModal')
+const btHamburguer = document.getElementById('btHamburguer')
+const navPhone = document.getElementById('navPhone')
+const fecharPhone = document.getElementById('fecharPhone')
+
+//Insere txtData dentro do ID "data"
+spanData.innerText = txtData
+
+
+//AÇÕES DO USUÁRIO (eventos)
+
+//Ao clicar no id "btFechar"
+// Fecha o modal
+btFechar.addEventListener('click', function(){
+    //oculta o divModal - CSS display valor none (por javascript)
+    divModal.style.display = 'none'
+
+})
+    
+// Abrir o menu hamburguer
+btHamburguer.addEventListener('click', function(){
+    navPhone.style.display = 'block'
+})
+
+// Fechar o menu hamburguer
+fecharPhone.addEventListener('click', function(){
+    navPhone.style.display = 'none'
+})
+
